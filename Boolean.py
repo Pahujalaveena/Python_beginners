a064b075dbb232a97196915b5bfcb78956582c9e
Laveena Pahuja <113932098+Pahujalaveena@users.noreply.github.com>
2023-09-29 19:17:18 +0100

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,38 @@
+
+#Boolean values
+x=True
+y=False
+print(x+y)
+print(x*y)
+print()
+
+
+#Boolean Conversion
+x=1
+y=0
+z=""
+print(bool(x))
+print(bool(y))
+print(bool(z))
+print()
+
+#Some Other Operations
+print(True==False)
+print(True!=False)
+print(True>=False)
+print(True<=False)
+print(True*False)
+print(True+False)
+print()
+
+#Similarly can be applied with "False"
+print(True==True)
+print(True!=True)
+print(True>=True)
+print(True<=True)
+print(True*True)
+print(True+True)
+print(True+2)
+print(True-1)
+print(True-2)
+print(True==1)
